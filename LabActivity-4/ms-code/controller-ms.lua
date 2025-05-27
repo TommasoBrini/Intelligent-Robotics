89357c5d4771fe64259a45ad9182190fc252bf41
TommasoBrini <tommaso.brini@studio.unibo.it>
2025-05-27 14:52:29 +0000

    solved ex4

--- LabActivity-4/ms-code/controller-ms.lua
+++ LabActivity-4/ms-code/controller-ms.lua
@@ -1,19 +1,15 @@
 -- import vector
 local vector = require "vector"
 
-
--- Put your global variables here
-
-
-MOVE_STEPS = 15
 MAX_VELOCITY = 15
+PRIORITY_LIGHT = 1.5
+PRIORITY_OBSTACLE = 2.0
 
 --[[ This function is executed every time you press the 'execute'
      button ]]
 function init()
 	robot.wheels.set_velocity(MAX_VELOCITY,MAX_VELOCITY)
 	L = robot.wheels.axis_length
-	n_steps = 0
 	robot.leds.set_all_colors("black")
 end
 
@@ -21,11 +17,9 @@ end
 --[[ This function is executed at each time step
      It must contain the logic of your controller ]]
 function step()
-	n_steps = n_steps + 1
 	
 	-- schema for the light
 	light_vector = {length = 0.0, angle = 0.0}
-	
 	max_light = 0
 	
 	for i=1, #robot.light do
@@ -37,22 +31,38 @@ function step()
 		end
 	end
 	
+	-- priority for light
+	light_vector.length = math.min(PRIORITY_LIGHT * light_vector.length, 1.0)
+	--light_vector.length = PRIORITY_LIGHT * light_vector.length
+	
 	-- schema for the obstacle
 	obstacle_vector = {length = 0.0, angle = 0.0}
 	
 	for i=1, #robot.proximity do
 		sensor = robot.proximity[i]
-		obstacle_vector.length = obstacle_vector.length + sensor.value
-		obstacle_vector.angle = obstacle_vector.angle + sensor.angle * sensor.value
+		-- take only sensor 5,4,3,2,1,24,23,22,21,20
+		if math.abs(sensor.angle) < 1.2 then
+			log(i)
+			obstacle_vector.length = obstacle_vector.length + sensor.value
+			obstacle_vector.angle = obstacle_vector.angle + sensor.angle * sensor.value
+		end
 	end
 	
 	if obstacle_vector.length > 0 then
 		obstacle_vector.angle = obstacle_vector.angle / obstacle_vector.length
-		obstacle_vector.length = - obstacle_vector.length
+		-- priority for obstacle avoidance
+		obstacle_vector.length = -PRIORITY_OBSTACLE * obstacle_vector.length	
 	end
 	
-	-- sum
+	-- sum	
 	result = vector.vec2_polar_sum(light_vector, obstacle_vector)
+	
+	--se ci sono ostacoli rallenta
+	log(obstacle_vector.length)
+	if obstacle_vector.length < 0 then
+    local slowdown = math.max(0.3, 1.0 + obstacle_vector.length)  -- tra 0.3 e 1.0
+    	result.length = result.length * slowdown
+    end
 	
 	-- matrix for calculate wheels velocity
 	v = result.length * MAX_VELOCITY
@@ -60,6 +70,15 @@ function step()
 	
 	vl = v - L/2 * w
 	vr = v + L/2 * w
+	
+	-- near light, stop
+	if max_light > 0.55 then
+		vl = 0
+		vr = 0
+		robot.leds.set_all_colors("green")
+	else
+       	robot.leds.set_all_colors("yellow")
+    end
 	
 	robot.wheels.set_velocity(vl, vr)
 	
@@ -74,8 +93,6 @@ end
      automatically by ARGoS. ]]
 function reset()
 	robot.wheels.set_velocity(MAX_VELOCITY,MAX_VELOCITY)
-	L = robot.wheels.axis_length
-	n_steps = 0
 	robot.leds.set_all_colors("black")
 end
 
